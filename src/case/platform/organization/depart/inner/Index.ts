dbc603b1291afbe3a1863a6868f4477c8b985b56
shsyang <987952354@qq.com>
2019-09-25 00:37:31 +0800

    delete

--- src/case/platform/organization/depart/inner/Index.ts
+++
@@ -1,9 +0,0 @@
-export * from './modal/AddModal';
-export * from './modal/DeleteModal';
-export * from './modal/SealModal';
-export * from './modal/UnSealModal';
-export * from './modal/MergeModal';
-export * from './modal/ImportModal';
-
-export * from './DepartTableColumns';
-export * from './OrgFlow';
