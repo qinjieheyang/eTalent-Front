5033905bdb8c1cd6a8bf318d7f7914523316115e
shsyang <987952354@qq.com>
2019-09-30 14:56:44 +0800

    update

--- src/case/platform/organization/depart/async/orgPanel/ServiceMock.ts
+++ src/case/platform/organization/depart/async/orgPanel/ServiceMock.ts
@@ -7,29 +7,6 @@ export class ServiceMock {
     //根据是否封存查询用户下所有的机构,图形化展示
     public getOrganizationGraphics = async (): Promise<any[]> => {
 
-        // const { list } = Mock.mock({
-        //     "list|10": [{
-        //         key: "@id",
-        //         companyId: "@id",
-        //         createTime: "@date",
-        //         isEnable: true, //是否启用
-        //         operatorId: "@id",
-        //         orgCode: generateOrgCode(), //机构编码
-        //         orgFullname: "机构全称-@integer(1, 100)", //机构全称
-        //         orgId: "@id", //机构ID
-        //         orgManagerId: "@id", //机构负责人
-        //         orgManagerName: "负责人-@integer(1, 100)", //机构负责人
-        //         orgName: "机构名称-@integer(1, 100)", //机构名称
-        //         orgParentId: "@id", //父级机构ID
-        //         orgParentName: "父级-@integer(1, 100)", //父级机构ID
-        //         "orgType|1": ["集团", "单位", "部门"],  //机构类型 ["集团", "单位", "部门"],
-        //         "color|+1": ["#FF8C58", "#2FDD93", "#19ADE6"],
-        //         total: 20,
-        //         online: 10,
-        //         sortId: "@increment", //序号
-        //     }]
-        // });
-
         const list = [
             { isEnable: true, orgCode: "1", orgId: 0, avatar: "/user.png", orgName: "中国雄安投资集团", orgManagerName: "张三", total: 20, online: 10, color: "#FF8C58" },
             { isEnable: true, orgCode: "1001", orgId: 1, orgParentId: 0, avatar: "/user.png", orgName: "集团办公室", orgManagerName: "李四", total: 20, online: 10, color: "#2FDD93" },
